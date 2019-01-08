15dc9542cd084c0d979762464b77dba8437e5046
Edoardo Carlesi <eduardo@l1.(none)>
2019-01-08 19:32:20 +0100

    local

--- scripts/test_fullbox.sh
+++ scripts/test_fullbox.sh
@@ -6,6 +6,6 @@ mpiexec='/z/eduardo/CLUES/libs/bin/mpiexec'
 cd ..; make; cd -
 rm ../tmp/*.tmp
 
-#$mpiexec -n 12 ../bin/MetroCPP ../config/fullbox_leibniz.cfg	
-$mpiexec -n 4 ../bin/MetroCPP ../config/fullbox_1024.cfg
+$mpiexec -n 12 ../bin/MetroCPP ../config/fullbox_leibniz.cfg	
+#$mpiexec -n 4 ../bin/MetroCPP ../config/fullbox_1024.cfg
 #$mpiexec -n 4 ../bin/MetroCPP ../config/fullbox_laptop.cfg	# Laptop setting
