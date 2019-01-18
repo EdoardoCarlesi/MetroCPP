f134c074443d52fa362980864be7193533528d4f
Edoardo Carlesi <gatto@nanowar.it>
2019-01-18 14:25:22 +0100

    Untracking some files

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#make clean; make
-n_cpus=12
-
-rm bin/*
-make
-mpirun -n $n_cpus ../bin/MetroCPP ../config/fullbox_1024.cfg
-#mpirun -n $n_cpus ./bin/MetroCPP config/config_template.cfg
